84879284ab93516a08905843d1854d58674e2c2d
shadabreyaz <shadabreyaz13@gmail.com>
2025-09-08 18:35:54 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,30 @@
+// for hamburger
+const hamburger = document.getElementById("hamburger");
+const menu = document.getElementById("menu");
+
+hamburger.addEventListener("click", () => {
+  menu.classList.toggle("active");
+  hamburger.classList.toggle('open');
+});
+
+// Close menu when click outside
+document.addEventListener('click', (event) => {
+  const isClickInsideToggle = hamburger.contains(event.target);
+  const isClickInsideMenu = menu.contains(event.target);
+
+  if (!isClickInsideToggle && !isClickInsideMenu) {
+    menu.classList.remove('active');
+    hamburger.classList.remove('open');
+  }
+});
+
+// Navbar scroll effect
+window.addEventListener('scroll', () => {
+  const navbar = document.getElementById('nav-container');
+
+  if (window.scrollY > 50) {
+      navbar.classList.add('scrolled');
+  } else {
+      navbar.classList.remove('scrolled');
+  }
+});
