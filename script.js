bf262e5f098803578cc09014b948be50dba28f5a
shadabreyaz <shadabreyaz13@gmail.com>
2025-09-09 22:40:12 +0530

    fixed other sections

--- script.js
+++ script.js
@@ -28,3 +28,32 @@ window.addEventListener('scroll', () => {
       navbar.classList.remove('scrolled');
   }
 });
+
+
+// FAQ script
+const faqItems = document.querySelectorAll('.faq-item');
+
+faqItems.forEach(item => {
+    item.querySelector('.faq-question').addEventListener('click', () => {
+        faqItems.forEach(i => {
+            if (i !== item) i.classList.remove('active');
+        });
+        item.classList.toggle('active');
+    });
+});
+
+// for animation
+  const observer = new IntersectionObserver((entries, observer) => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add('in-view');
+        observer.unobserve(entry.target); // Animate only once
+      }
+    });
+  }, {
+    threshold: 0.2
+  });
+
+  document.querySelectorAll('.animate-on-scroll').forEach(el => {
+    observer.observe(el);
+  });
